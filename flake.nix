901151de3c6d89ac77d18e173491ac7e9f5684fc
practicalmethod-sandiego <practicalmethod-sandiego@proton.me>
2026-02-02 00:58:09 -0800

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  nixConfig = {
+    extra-substituters = "https://cache.nixos.asia/oss";
+    extra-trusted-public-keys = "oss:KO872wNJkCDgmGN3xy9dT89WAhvv13EiKncTtHDItVU=";
+  };
+
+  inputs = {
+    emanote.url = "github:srid/emanote";
+    emanote.inputs.emanote-template.follows = "";
+    nixpkgs.follows = "emanote/nixpkgs";
+    flake-parts.follows = "emanote/flake-parts";
+  };
+
+  outputs = inputs@{ self, flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ inputs.emanote.flakeModule ];
+      perSystem = { self', pkgs, system, ... }: {
+        emanote = {
+          # By default, the 'emanote' flake input is used.
+          # package = inputs.emanote.packages.${system}.default;
+          sites = rec {
+            # For available options, see:
+            # https://github.com/srid/emanote/blob/master/nix/modules/flake-parts/flake-module/site/default.nix
+            default = {
+              layers = [{ path = ./.; pathString = "."; }];
+              # port = 8080;
+            };
+            # Optimized for deploying to https://<user>.github.io/<repo-name> URLs
+            github-io = default // {
+              check = false;
+              extraConfig.template.baseUrl = "/emanote-template/";
+            };
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nixpkgs-fmt
+          ];
+        };
+        formatter = pkgs.nixpkgs-fmt;
+      };
+    };
+}
